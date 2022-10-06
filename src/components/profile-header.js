f4ea50105b7067185ff7d659fb1492562b1d265d
Spencer Cheng <89827794+spencercheng@users.noreply.github.com>
2022-10-05 19:53:51 -0600

    3111 Display Initials (HEADER)

--- src/components/profile-header.js
+++ src/components/profile-header.js
@@ -1,9 +1,10 @@
 import underlineSrc from "../assets/underline.svg";
 
 export const updateProfileInformation = (data) => {
-  const { firstName, lastName, avatarSrc } = data;
+  const { firstName, lastName, avatarSrc} = data;
   const headerNode = document.querySelector("#profile-header .profile-header");
   const profileAvatarNode = headerNode.querySelector("img");
+  const profileNameNode = headerNode.querySelector(".profile-avatar");
   const nameNode = headerNode.querySelector(".profile-info .profile-info-name");
   const underlineNode = headerNode.querySelector(".profile-underline");
 
@@ -15,12 +16,29 @@ export const updateProfileInformation = (data) => {
     "skeleton-block--half"
   );
 
+  if (avatarSrc) {
+    const avatarImg = document.createElement("img");
+    avatarImg.src = avatarSrc;
+    avatarImg.setAttribute("aria-label", `${firstName} ${lastName}`);
+    profileAvatarNode.appendChild(avatarImg);
+   } else {
+    profileAvatarNode.remove()
+    var initials = firstName.substring(0, 1).toUpperCase() + lastName.substring(0, 1).toUpperCase();
+
+    console.log(initials);
+  
+    const avatarImg = document.createElement("div");
+    avatarImg.textContent = `${initials}`;
+    profileNameNode.appendChild(avatarImg);
+    avatarImg.setAttribute("style",  "text-align: center; font-size: 4vh; color: white; margin-top:3vh;")
+  
+  }
+
   nameNode.innerHTML = `${firstName} ${lastName}`;
   nameNode.appendChild(underlineNode);
-  profileAvatarNode.src = avatarSrc;
-  profileAvatarNode.setAttribute("aria-label", `${firstName} ${lastName}`);
 
-  if (!avatarSrc) {
-    profileAvatarNode.remove();
-  }
+
+  // if (!avatarSrc) {
+  //   profileAvatarNode.remove();
+  // }
 };
